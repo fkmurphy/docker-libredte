566f723e45a256896265d807124f7ea263ff69d9
fkmurphy <njmdistrisoft@gmail.com>
2020-11-09 01:01:43 -0300

    configuration core

--- libredte/start.sh
+++ libredte/start.sh
@@ -1,7 +1,17 @@
 #!/bin/sh
 
-[ ! $UID -eq 0 ] && exit 1;
+#[ ! $UID -eq 0 ] && exit 1;
 
-cd /app && git clone --recursive https://github.com/LibreDTE/libredte-webapp.git .
+#cd /app && git clone --recursive https://github.com/LibreDTE/libredte-webapp.git .
 
+#cd /app/website && composer install
 
+#CONFIG_FILE="/app/website/Config/core.php"
+
+#cp "/install_ldte/core-dist.php" $CONFIG_FILE 
+
+#sed -i -e "s/%DB_USER%/${DB_USER}/g" \
+#       -e "s/%DB_PASSWORD%/${DB_PASSWORD}/g" \
+#       -e "s/%DB_HOSTNAME%/${DB_HOSTNAME}/g" \
+#       -e "s/%DB_NAME%/${DB_NAME}/g" \
+#    $CONFIG_FILE
